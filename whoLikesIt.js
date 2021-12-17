1136031f91460cc50c4b21c722052ec4cd248dfc
Jenn <hashbrowns120@gmail.com>
2021-12-17 11:35:38 -0600

    updated who likes it? challenge

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -16,21 +16,21 @@ Note: For 4 or more names, the number in "and 2 others" simply increases.
 
 function likes(names) {
     if (names.length === 0){
-      return 'no one likes this';
-    }
-    for (let i = 0; i < names.length; i++){
-      if (names.length === 1){
-        return `${names[0]} likes this`
-      } else if (names.length === 2){
-        return `${names[0]} and ${names[1]} like this`
-      } else if (names.length === 3){
-        return `${names[0]}, ${names[1]} and ${names[2]} like this`
-      } 
-      if (names.length >= 4){
-        return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
-      }
+    return 'no one likes this';
+  }
+  if (names.length === 1){
+    return `${names[0]} likes this`
+  } else if (names.length === 2){
+    return `${names[0]} and ${names[1]} like this`
+  } else if (names.length === 3){
+    return `${names[0]}, ${names[1]} and ${names[2]} like this`
+  } 
+  for (let i = 0; i < names.length; i++){
+    if (names.length >= 4){
+      return `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
     }
   }
+}
 
   /*
   describe('example tests', function() {
